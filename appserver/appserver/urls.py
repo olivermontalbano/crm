df8264b5b464ada91461e20e02044cfe484fb210
Oliver Montalbano <35673534+olivermontalbano@users.noreply.github.com>
2025-02-18 10:50:16 -0800

    Fix dependencies so admin and swagger ui running

--- appserver/appserver/urls.py
+++ appserver/appserver/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework import permissions
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -28,7 +30,8 @@ schema_view = get_schema_view(
         license=openapi.License(name="MIT License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
+
 )
 
 urlpatterns = [
